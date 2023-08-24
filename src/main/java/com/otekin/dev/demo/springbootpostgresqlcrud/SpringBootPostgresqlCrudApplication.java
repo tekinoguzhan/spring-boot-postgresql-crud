c4b4b6cbf35955268ce0452b402f40739c6d13cc
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-08-24 04:14:43 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/demo/springbootpostgresqlcrud/SpringBootPostgresqlCrudApplication.java
@@ -0,0 +1,14 @@
+package com.otekin.dev.demo.springbootpostgresqlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+		  SpringBootPostgresqlCrudApplication.class, args);
+	}
+
+}
